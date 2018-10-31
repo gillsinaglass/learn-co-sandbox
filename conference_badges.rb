0d2e897bc0f8baee947f9590e25f39a450c20ac4
Ryan Gill <rdgill95@gmail.com>
2018-10-31 17:58:08 +0000

    automatically backed up by learn

---
+++ conference_badges.rb
@@ -0,0 +1,18 @@
+def badge_maker(name)
+  return "Hello my name is #{name}."
+end
+
+def batch_badge_creator(speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+  speaker.each do |name|
+    return badge_messages = ["Hello my name is #{speaker[0]}.","Hello my name is #{speaker[1]}.","Hello my name is #{speaker[2]}." ,"Hello my name is #{speaker[3]}.","Hello my name is #{speaker[4]}.","Hello my name is #{speaker[5]}.","Hello my name is #{speaker[6]}."]
+  end
+end
+
+def assign_rooms(speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"], rooms = ["1", "2", "3", "4", "5", "6", "7"])
+  speaker.each do |name|
+    return "Hello, #{name}! You've been assigned room #{rooms[0]}!"
+    rooms.shift
+  end
+end
+  
+  
